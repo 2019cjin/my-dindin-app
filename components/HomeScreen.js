7287c9170d6f097bd4d8e8534e5a4fa6eafbbb11
esb5er <esb5er@virginia.edu>
2019-02-21 16:32:38 -0500

    Homescreen header

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,13 +1,71 @@
 import * as React from 'react';
-import { Text, View} from 'react-native';
+import { Constants } from 'expo'
+import { Text, View, StyleSheet, Image} from 'react-native';
 //import { listenOrientationChange, removeOrientationListener, widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default class HomeScreen extends React.Component{
     render(){
         return(
-            <View>
-                <Text>HomeScreen</Text>
-            </View>
+           <View style={styles.container}>
+           <View style={styles.header}>
+           <Image style = {styles.sideBtn} source ={require   ('./assets/sidemenu btn@2x.png')}>
+            </Image>
+
+            <Text style = {styles.title}> DinDin </Text>
+
+
+             <Image style = {styles.searchBtn} source ={require   ('./assets/search btn@2x.png')}>
+            </Image>
+
+           </View>
+
+            </View>            
+           
         )
     }
 }
+
+const styles = StyleSheet.create({
+
+  container: {
+            flex: 1,
+            flexDirection:'column',
+            backgroundColor: "white",
+            paddingTop: Constants.statusBarHeight,
+
+        },
+
+  
+ 
+      header: { 
+      flex: 1,
+      alignItems: 'flex-start',
+      justifyContent: 'space-between',
+      backgroundColor: 'white',
+      flexDirection:'row',
+      },
+
+      sideBtn: { 
+      width:32,
+      height:30,
+   },
+
+   title:{
+
+     textAlignment: "Center",
+      verticalAlignment:"Center",
+      lineHeight:23,
+      textWrapping:"Wrap",
+      //Text="DinDin",
+      fontFamily:"Segoe UI",
+      fontSize:17,
+      fontStretch:"Medium",
+      foreground:"#353535",
+
+   },
+
+   searchBtn: { 
+      width:40,
+      height:40,
+   },
+});
---
+++ components/PendingInvite.js
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import {View, Text, StyleSheet,ImageBackground} from 'react-native'
+
+export default class PendingInvite extends React.Component{
+
+ constructor(props){
+     super(props)
+     this.state={
+         featured: null
+     }
+ }   
+
+componentDidMount(){
+    this.getData()
+}
+
+/*
+async getData(){
+    let response = await fetch("https://www.cs.virginia.edu/~dgg6b/Mobile/Featured/featured.json")
+    let parsedResponse = await response.json()
+    this.setState({
+        featured : parsedResponse
+    })
+}*/
+
+render(){
+    if(this.state.featured === null){
+        return(<View/>)
+    }
+    return(
+        
+        <View style={styles.container}>
+
+        <ImageBackground style={styles.featuredImage}
+        source={{uri: this.state.featured.image}}>
+            <Text style={styles.title}> {this.state.featured.title}
+             </Text>
+            <Text style={styles.author}> {this.state.featured.author} 
+            </Text>
+        </ImageBackground>
+        </View>
+        )
+    }
+}
+const styles = StyleSheet.create(
+    {
+        container:{
+            height:158,
+            margin: 10,
+        },
+        featuredImage:{
+            height: 148,
+            flexDirection: 'column',
+            justifyContent: 'flex-end',
+        },
+        title:{
+            fontFamily: "Helvetica-Bold",
+            fontSize: 20,
+            color: "#FFFFFF",
+            letterSpacing: 0.38,
+            textAlign: "left",
+        }, 
+        author:{
+            fontFamily: "Helvetica",
+            fontSize: 16,
+            color: "rgba(255,255,255,0.75)",
+            textAlign: "left"
+        }
+
+       
+    }
+)
