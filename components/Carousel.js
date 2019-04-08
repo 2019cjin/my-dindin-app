842c6622c92ba189d0e03d03e8c9c43ace10054b
esb5er <47600651+esb5er@users.noreply.github.com>
2019-04-08 00:44:12 -0400

    small changes

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,25 +1,72 @@
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
 import { Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
-//source ={{uri: item.profilePic}}
+import Carousel from 'react-native-snap-carousel';
 
-export default class InvitesCarousel extends React.Component
-{
-  render(){
-    return(
-      <View style={{height:300, width: 300}}>
-      <Text>Carousel</Text>
-      <Carousel>
+const firebaseConfig = {
+    apiKey: "AIzaSyDZpTrKnBHgaQbv_F87VoD5ZOn83Rkqe-w",
+    authDomain: "dindin-9954b.firebaseapp.com",
+    databaseURL: "https://dindin-9954b.firebaseio.com",
+    projectId: "dindin-9954b",
+    storageBucket: "dindin-9954b.appspot.com",
+    messagingSenderId: "1055947992772"
+  };
+  
 
-        <View><Text>Text 1</Text></View>
-        <View><Text>Text 2</Text></View>
-        <View><Text>Text 3</Text></View>
-      </Carousel>
-      </View>
-    )
-  }
+
+
+export class InviteCarousel extends Component {
+
+    constructor(props){
+        super(props)
+    }
+
+    startListener(path) {
+        let context = this
+        firebase.database().ref(path).on('value', (snapshot) => {     
+          console.log(JSON.stringify(snapshot.val()))
+          context.setState({
+            data: JSON.parse(JSON.stringify(snapshot.val())), 
+            gotInformation: true
+          })
+        })
+      }
+
+    _renderItem ({item, index}) {
+        return (
+            <View>
+                <Text> Testing </Text>
+            </View>
+        );
+    }
+
+    render () {
+        return (
+            <Carousel 
+              layout={'default'}            
+              ref={(c) => { this._carousel = c; }}
+              data={this.state.data[0]["name"]}
+              renderItem={this._renderItem}
+            />
+        );
+    }
 }
+
+
+
+/*
+ layout={'default'}            
+              ref={(c) => { this._carousel = c; }}
+              data={this.state.data[0]["name"]}
+              renderItem={this._renderItem}
+              sliderWidth= '100'
+              itemWidth='10'
+*/
+
+
+
+
+
 
 
 /*<Carousel>
